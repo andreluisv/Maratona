2f45b32c5a42b786924d283f410223a4c80f93ff
André Vasconcelos <42820943+alpvj@users.noreply.github.com>
2020-09-07 21:11:00 -0300

    Código para C(n,k)

---
+++ Code/Math/Combination.cpp
@@ -0,0 +1,28 @@
+const ll mod = 1e9+7;
+vector<ll>fat;
+
+void fill_fat(int n){
+	fat=vector<ll>(n+1,1);
+	for(ll i=1;i<=n;i++){
+		fat[i]=(fat[i-1]*i)%mod;
+	}
+}
+ 
+ll binExp(ll b, ll e){
+	ll ret=1;
+	while(e){
+		if(e&1){
+			ret=ret*b%mod;
+		}
+		b=b*b%mod;
+		e/=2;
+	}
+	return ret;
+}
+ 
+ll comb(ll n, ll k){
+	ll ret = fat[n]*binExp(fat[k],mod-2)%mod;
+	ret=ret*binExp(fat[n-k],mod-2)%mod;
+ 
+	return ret;
+}
