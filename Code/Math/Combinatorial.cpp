7dc26cab3ae4300b9a92201af9e92f75b48d1e92
André Vasconcelos <42820943+alpvj@users.noreply.github.com>
2020-09-07 21:28:09 -0300

    Improving time by saving k!⁽⁻¹⁾

--- Code/Math/Combinatorial.cpp
+++ Code/Math/Combinatorial.cpp
@@ -1,13 +1,18 @@
+typedef long long ll;
 const ll mod = 1e9+7;
-vector<ll>fat;
+vector<ll>fat,inv; 
+// inv[k] = modular inverse of k! mod
+//a^(phi(mod)-1) = a^-1 mod
+//a^(mod-2)=a^-1 mod
 
 void fill_fat(int n){
 	fat=vector<ll>(n+1,1);
+	inv=vector<ll>(n+1,-1);
 	for(ll i=1;i<=n;i++){
-		fat[i]=(fat[i-1]*i)%mod;
+		fat[i]=fat[i-1]*i %mod;
 	}
 }
- 
+
 ll binExp(ll b, ll e){
 	ll ret=1;
 	while(e){
@@ -19,12 +24,13 @@ ll binExp(ll b, ll e){
 	}
 	return ret;
 }
- 
+
 ll comb(ll n, ll k){
-	ll ret = fat[n]*binExp(fat[k],mod-2)%mod;
-	ret=ret*binExp(fat[n-k],mod-2)%mod;
- 
-	return ret;
+	if(inv[k]==-1)inv[k]=binExp(fat[k],mod-2);
+	if(inv[n-k]==-1)inv[n-k]=binExp(fat[n-k],mod-2);
+
+	ll ret = fat[n]*inv[k]%mod;
+	return ret = ret*inv[n-k]%mod;
 }
 
 ll arra(ll n, ll k){
